6a8034bcaee34f16b323c31de8144bd72170d370
creaven <creavenmoro@gmail.com>
2010-02-10 16:41:43 +0300

    fix css generator

--- 9css.py
+++ 9css.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
-f=open('result.css', 'w')
+f=open('/Users/moro/tmp/result.css', 'w')
 
 cls='mif-menu-bg'
 ie6='.ie6'
 
 
-coords=(46, 43, 40, 44)
+coords=(45, 35, 45, 45)
 
 left, top, right, bottom = map(str, coords)
 
 padding_left='40'
-padding_top='37'
+padding_top='47'
 l='-19'
 t='-17'
 
@@ -35,8 +35,8 @@ css+='.'+cls+', .'+cls+' div{\n'+'position: absolute;\n'+'overflow:hidden;\n'+'}
 css+='.'+cls+' {\n'+'left:'+l+'px;\n'+'top:'+t+'px;\n'+'padding-left:'+padding_left+'px;\n'+'padding-top:'+padding_top+'px;\n'+'}\n'
 #css+=ie6+' '+'.'+cls+' {\n'+'overflow-y:hidden;\n'+'}\n'
 css+='.'+cls+' .top{\n'+'height: '+top+'px;\n'+'width: 100%;\n'+'position: relative;\n'+'top:-'+padding_top+'px;\n'+'padding-left:'+padding_left+'px;\n'+'padding-top:'+padding_top+'px;\n'+'left:-'+padding_left+'px;\n'+'}\n'
-css+='.'+cls+' .center{\n'+'height: 100%;\n'+'width: 100%;\n'+'position: relative;\n'+'top:-'+str(2*int(bottom)+2*int(padding_top))+'px;\n'+'padding-left:'+padding_left+'px;\n'+'padding-top:'+padding_top+'px;\n'+'left:-'+padding_left+'px;\n'+'}\n'
-css+='.'+cls+' .bottom{\n'+'height: '+bottom+'px;\n'+'width:100%;\n'+'top:-'+str(2*int(bottom)+2*int(padding_top))+'px;\n'+'position:relative;\n'+'padding-left:'+padding_left+'px;\n'+'left:-'+padding_left+'px;'+'}\n'
+css+='.'+cls+' .center{\n'+'height: 100%;\n'+'width: 100%;\n'+'position: relative;\n'+'top:-'+str(int(bottom)+int(top)+2*int(padding_top))+'px;\n'+'padding-left:'+padding_left+'px;\n'+'padding-top:'+padding_top+'px;\n'+'left:-'+padding_left+'px;\n'+'}\n'
+css+='.'+cls+' .bottom{\n'+'height: '+bottom+'px;\n'+'width:100%;\n'+'top:-'+str(int(bottom)+int(top)+2*int(padding_top))+'px;\n'+'position:relative;\n'+'padding-left:'+padding_left+'px;\n'+'left:-'+padding_left+'px;'+'}\n'
 css+='.'+cls+' .tl{\n'+'width: '+left+'px;\n'+'height: '+top+'px;\n'+'background:'+'url("'+imgs['tl']+'")'+';\n'+'left:0px;\n'+'top:0px;\n'+'}\n'
 css+='.'+cls+' .tr{\n'+'width: '+right+'px;\n'+'height: '+top+'px;\n'+'float:right;\n'+'position: relative;\n'+'background:'+'url("'+imgs['tr']+'");\n'+'top:-'+padding_top+'px;\n'+'}\n'
 css+='.'+cls+' .t{\n'+'height: '+top+'px;\n'+'width:100%;\n'+'left:-'+str(int(right)+int(padding_left))+'px;\n'+'top: 0;\n'+'clip:rect(auto auto auto '+str(int(left)+int(right)+int(padding_left))+'px);\n'+'background:'+'url("'+imgs['t']+'")'+';\n'+'padding-left:'+padding_left+'px;\n'+'}\n'
